6615057b1a6cb0aac76daab51b602be23f89f6d0
Kevin Müller <kevin.mueller1@outlook.com>
2022-05-07 21:15:35 +0200

    implemented api

---
+++ NextDayOff/Models/Day.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace NextDayOff.Models
+{
+    public class Day
+    {
+        [JsonProperty("date")]
+        public string? Date { get; set; }
+
+        [JsonProperty("localName")]
+        public string? LocalName { get; set; }
+
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+
+        [JsonProperty("countryCode")]
+        public string? CountryCode { get; set; }
+
+        [JsonProperty("fixed")]
+        public bool Fixed { get; set; }
+
+        [JsonProperty("global")]
+        public bool Global { get; set; }
+
+        [JsonProperty("counties")]
+        public List<string>? Counties { get; set; }
+
+        [JsonProperty("launchYear")]
+        public int? LaunchYear { get; set; }
+
+        [JsonProperty("types")]
+        public List<string>? Types { get; set; }
+    }
+}
